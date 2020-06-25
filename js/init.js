72518e233b42f3f4d77af59e5fe6ec93a9bc13ab
Andrew Boskovich <AndrewBoskovich@0587397691.wireless.umich.net>
2020-06-25 19:36:07 -0400

    try

--- js/init.js
+++ js/init.js
@@ -4,45 +4,6 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-// //svg animation
-// $(document).ready(function() {
-//   var tmax_tl = new TimelineMax({
-//       delay: 2,
-//       repeat: 0,
-//       repeatDelay: 2
-//     }),
-
-//     svg_shapes = $('#logo'),
-//     stagger_val = 0.00125,
-//     duration = 1.5,
-
-//     stagger_opts_from = {
-//       css: {
-//         opacity: 0,
-//         transform: 'translate(0px, 50px) rotate(0deg) scale(1)',
-//         transformOrigin: 'center center'
-//       },
-//       ease: Elastic.easeInOut
-//     },
-
-//     stagger_opts_to = {
-//       css: {
-//         opacity: 1,
-//         transform: 'translate(0px) rotate(0deg) scale(1.2)'
-//       },
-//       ease: Elastic.easeInOut
-//     };
-
-//   tmax_tl.staggerFromTo(
-//     svg_shapes,
-//     duration,
-//     stagger_opts_from,
-//     stagger_opts_to,
-//     stagger_val,
-//     0
-//   );
-// });
-
 //On clicking the mooments button on projects page, change to moments tab
 $(".moments-btn").click(function() {
     $('ul.tabs').tabs('select_tab', 'moments');
@@ -69,12 +30,12 @@ $("#work-link").click(function() {
 });
 
 var figure = $(".video").hover( hoverVideo, hideVideo );
-function hoverVideo(e) {  
-    $(this).get(0).play(); 
+function hoverVideo(e) {
+    $(this).get(0).play();
 }
 
 function hideVideo(e) {
-    $(this).get(0).pause(); 
+    $(this).get(0).pause();
 }
 
 
@@ -107,14 +68,14 @@ var validatepsswrd = function(event) {
     if(txt == "access2017") {
       input.addClass("valid");
       window.open(project, '_blank');
-      
+
       $(".psswrd").hide();
       $(".access-link").hide();
 
       input.removeClass("invalid");
-    
+
     } else if(!(txt == "access2017") && event.keyCode == 13) {
-      
+
       $(".access-link").show();
       input.addClass("invalid");
 
